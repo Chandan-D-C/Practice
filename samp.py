d96d11f4142df1f93704d978a0357d943d27ae93
Chandan D C <chandan20041220@gmail.com>
2026-09-21 21:29:09 +0530

    Refactor code to handle employee bonuses
    Replace list operations with employee and bonus dictionaries.

--- samp.py
+++ samp.py
@@ -1,4 +1,14 @@
-a=[1,2,3,4,5]
-print(a[0:3])
-a.append(6)
-print(a)
+employee = [
+    [3, "Brad"],
+    [1, "John"],
+    [2, "Dan"]
+]
+
+bonus = {
+    2: 500,
+    4: 2000
+}
+
+for emp_id, name in employee:
+    if emp_id not in bonus or bonus[emp_id] < 1000:
+        print(name, bonus.get(emp_id))
